aa0467fc5b786aaae5f4e88328d0ebb98581ee41
benruiz1024-ops <benruiz1024@gmail.com>
2025-12-18 15:59:56 -0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+// Minimal service worker for offline caching (works when hosted on HTTPS).
+const CACHE = "tinywriter-iphone-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-180.png"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => (k !== CACHE) ? caches.delete(k) : null)))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
